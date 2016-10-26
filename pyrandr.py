92b8fa3bddb5074750cddd9d2f6b66d3922e9b9a
rarellano <rafa.are.ro@gmail.com>
2016-10-26 17:29:39 +0200

    cleaning code

--- pyrandr.py
+++ pyrandr.py
@@ -8,23 +8,18 @@ def command_out(cmd):
 	c = run(cmd, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
 	return c.stdout
 
-
 def find_devices(stdout):
 	devices=[]
-
 	for i in cmdout:
 		if 'connected' in i and not 'disconnected' in i:
 			aux=i.split(' ')
 			devices.append(aux[0])
-	
 	return devices
-
 
 def xrandr_exec(devices):
 	a = devices[0]
 	b = devices[1]
 	os.system('xrandr --output '+ a +' --auto  --output '+ b +' --right-of '+ a)
-
 
 cmdout = command_out('xrandr').split('\n')
 
