1f06d59c1dc7d9d8d133f22b5b8041bc63c5ffdd
rarellano <rafa.are.ro@gmail.com>
2016-10-28 09:20:41 +0200

    added new display modes

--- pyrandr.py
+++ pyrandr.py
@@ -16,10 +16,33 @@ def find_devices(stdout):
 			devices.append(aux[0])
 	return devices
 
+def display_state(n):
+	# fstate = open('/home/rarellano/.config/','w')
+	pass
+
+def get_display_state(n):
+	pass
+
 def xrandr_exec(devices):
 	a = devices[0]
 	b = devices[1]
-	os.system('xrandr --output '+ a +' --auto  --output '+ b +' --right-of '+ a)
+	
+	display_state(0)
+	
+	if len(devices) == 2:
+		if get_display_state == 0:
+			os.system('xrandr --output '+ a +' --off')
+		if get_display_state == 1:
+			os.system('xrandr --output '+ b +' --off')
+		if get_display_state == 2:
+			os.system('xrandr --output '+ a +' --auto  --output '+ b +' --same-as '+ a)
+		if get_display_state == 3:
+			os.system('xrandr --output '+ a +' --auto  --output '+ b +' --left-of '+ a)
+		if get_display_state == 4:
+			os.system('xrandr --output '+ b +' --auto  --output '+ a +' --left-of '+ b)
+		
+		display_state(1)
+
 
 cmdout = command_out('xrandr').split('\n')
 
